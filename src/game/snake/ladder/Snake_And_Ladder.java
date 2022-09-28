0e524ba5c8f9a76962bca3f3d62d691522358f4c
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-09-28 07:04:54 +0530

    Added: Welcome Message

---
+++ src/game/snake/ladder/Snake_And_Ladder.java
@@ -0,0 +1,10 @@
+package game.snake.ladder;
+
+public class Snake_And_Ladder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("welcome to the snake and ladder program");
+	}
+
+}
