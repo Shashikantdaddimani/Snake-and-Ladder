ca69f4bbcc3da0dbf4981da99bcec89dcd5e692e
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-09-28 07:11:21 +0530

    Added:user Start position

--- src/game/snake/ladder/Snake_And_Ladder.java
+++ src/game/snake/ladder/Snake_And_Ladder.java
@@ -5,6 +5,13 @@ public class Snake_And_Ladder {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to the snake and ladder program");
+		int userPosition = 0;
+		
+		System.out.println("Initial position of user: "+userPosition);
+		
+		
+		
+		
 	}
 
 }
