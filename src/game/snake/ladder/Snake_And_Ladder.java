a3b48b7c464c6ecde4a3748902936aeddb7655f0
Shashikantdaddimani <shashi0344daddimani@gmail.com>
2022-09-28 07:23:08 +0530

    Added:Player rolls a die and get a random number

--- src/game/snake/ladder/Snake_And_Ladder.java
+++ src/game/snake/ladder/Snake_And_Ladder.java
@@ -1,16 +1,19 @@
 package game.snake.ladder;
 
+import java.util.Random;
+
 public class Snake_And_Ladder {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Random random = new Random();
 		System.out.println("welcome to the snake and ladder program");
 		int userPosition = 0;
 		
 		System.out.println("Initial position of user: "+userPosition);
 		
-		
-		
+		int dice = random.nextInt(6)+1;
+		System.out.println("player rolls a die and get a random number is: "+dice);
 		
 	}
 
